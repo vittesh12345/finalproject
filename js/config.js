a201a1014cf2ce2efaf53108f73bee0de882fed5
vittesh12345 <49050174+vittesh12345@users.noreply.github.com>
2025-06-22 18:46:18 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
     userPoolId: 'us-east-2_hlMHw18ZN',
-    userPoolClientId: '4f1tb850egv2sssjmnlu8qeea7',
+    userPoolClientId: '53ennqhknq2khecseemp6uln4p',
     region: 'us-east-2'
   },
   api: {
